1d5df9bf3e6546bbb6b6ec1f5598c3c19116d2ad
Jonathan Fisher <jfisher@eab.com>
2017-12-16 18:08:04 -0600

    update case study with more images and color swatch component

--- src/pages/historical.js
+++ src/pages/historical.js
@@ -1,13 +1,32 @@
 import React from 'react'
 import Link from 'gatsby-link'
 import Tachyons from 'tachyons'
+import styled from 'styled-components'
 import historicalOne from './historical/historical_one.svg'
 import historicalTwo from './historical/historicalTwo.png'
 import historicalThree from './historical/historicalThree.png'
+import trello_board from './historical/trello_board.png'
+import figma_draft from './historical/figma_draft.png'
+
+const ColorSwatchRed = styled.span`
+	width: 0.75rem;
+  height: 0.75rem;
+  display: inline-block;
+	background-color: #471D1D;
+  box-shadow: 0 1px 2px 0 gray;
+`;
+
+const ColorSwatchDarkRed = styled.span`
+	width: 0.75rem;
+  height: 0.75rem;
+  display: inline-block;
+	background-color: #2E0F0F;
+  box-shadow: 0 1px 2px 0 gray;
+`;
 
 
 const SecondPage = () => (
-  <div className="spectral measure-wide-ns center-ns">
+  <div className="spectral mw7-ns center-ns">
     <h1 className="mt5 mb2 f1">A Case Study: Historical Homecrafters</h1>
     <a href="http://historicalhomecrafters.com/"
       className="link dark-green dim fw7 spectral underline mt0">
@@ -21,7 +40,7 @@ const SecondPage = () => (
       as well as other builders.
     </p>
     <div className="w-100-ns">
-      <img className="mb0 mt4" src={historicalOne} />
+      <img className="mb0 mt4 shadow-5" src={historicalOne} />
     </div>
     <h3 className="fw7 f4 mb0 mt4">The Project</h3>
     <p className="f4 mb0 mt2">
@@ -53,9 +72,54 @@ const SecondPage = () => (
       apparent throughout the site.
     </p>
     <div className="w-100-ns mb0 mt4">
-      <img className="w-50-ns" src={historicalTwo} />
-      <img className="w-50-ns" src={historicalThree} />
+      <img className="w-50-ns shadow-5" src={historicalTwo} />
+      <img className="w-50-ns shadow-5" src={historicalThree} />
     </div>
+    <h3 className="fw7 f4 mb0 mt4">The Process</h3>
+    <p className="f4 mb0 mt2">
+      After listing out all the ideas that I’d like to apply to the site, I
+      began placing all of them against their worst enemy: the aforementioned
+      constraints and priorities. Many of them survived the battle, and I
+      worked to include the ones I felt had the largest value adds, knowing
+      that the smarter I executed, the more interesting stuff I could
+      include.
+    </p>
+    <div className="w-100-ns mb0 mt4">
+      <img className="w-100-ns shadow-5" src={trello_board} />
+    </div>
+    <h3 className="fw7 f4 mb0 mt4">The Type</h3>
+    <p className="f4 mb0 mt2">
+      Accessibility is important in order to account for large age-ranges,
+      so I value a large contrast ratio. Our main site colors (
+      <span className="i">#471D1D</span> and
+      <span className="i"> #FFFFD9</span>) have a 14.1:1 contrast ratio, which is good at any
+      reasonably-sized font size.
+    </p>
+    <p className="f4 mb0 mt2">
+      The fonts I use are Avenir and Athelas, a sans-serif and serif font,
+      respectively. Avenir is used for clear calls-to-action while Athelas is
+      mainly used for readably copy.
+    </p>
+    <h3 className="fw7 f4 mb0 mt4">The Color</h3>
+    <p className="f4 mb0 mt2">
+      The two main background colors that make up the site are a gradient with
+      small tweaks in saturation, and brightness. The hue however stayed the
+      same for both darker reds (#471D1D <ColorSwatchRed /> and #2E0F0F <ColorSwatchDarkRed />). This ends up helping the copy, through trick
+      of the eye, to jump off the screen a bit and gives the rendering
+      a sheen. This enabled me to give the site a newness without having to be
+      too adventurous for a client.
+    </p>
+    <div className="w-100-ns mb0 mt4">
+      <img className="w-100-ns shadow-5" src={figma_draft} />
+    </div>
+    <h3 className="fw7 f4 mb0 mt4">The Result</h3>
+    <p className="f4 mb0 mt2">
+      There are many things I continue to learn (e.g. man, should I have used a
+      static site generator to speed up this process and spend more time on
+      design decisions), but I unexpectedly enjoyed the tradeoffs I made in
+      order to serve the client.
+    </p>
+
     <Link to="/" className="link dark-green dim fw7 spectral underline dib mb0 mt4">\\ Back To Home</Link>
   </div>
 )
